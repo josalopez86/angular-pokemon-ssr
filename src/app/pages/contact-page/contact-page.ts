17fa234097599bf78c518840442049fc6c33b91f
josalopez86 <josa.lopez@live.com.mx>
2026-06-29 14:59:57 -0700

    first commit

---
+++ src/app/pages/contact-page/contact-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector:"contact-page",
+  templateUrl: "./contact-page.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ContactPage {}
